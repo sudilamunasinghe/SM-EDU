8971f7ccf4a56dd098b81bf578e3c27edcb48b57
sudila <sudilamunasinghe@gmail.com>
2023-06-05 20:26:26 +0530

    Created Home Slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,22 @@ $(".nav-close-icon").click(function(){
         display: 'block',
     })
 })
+
+
+// Home Slider
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+});
